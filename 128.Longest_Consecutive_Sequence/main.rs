f9b4afa0921d3351b992c198fe474e20a5ab3a9f
SAm NOur <sameh.nour.uni@outlook.com>
2024-04-17 00:23:45 +0200

    third

--- 128.Longest_Consecutive_Sequence/main.rs
+++ 128.Longest_Consecutive_Sequence/main.rs
@@ -1,36 +1,26 @@
 use std::collections::HashSet;
 use std::iter::FromIterator;
-
-fn main () {
-	let mut count = 1;
-	let res = vec![0,3,7,2,5,8,4,6,0,1];
-	let hs :HashSet<i32> = HashSet::from_iter(res);
-	println!("{:?}", hs);
-	for i in &hs{
-		let pre = (i + 1).abs();
-		if hs.contains(&pre) {
-			count += 1;
-		}
-
-	}
-	println!("{}", count);
-}
-
-/*
-use std::collections::HashSet;
-use std::iter::FromIterator;
+use std::cmp;
 impl Solution {
     pub fn longest_consecutive(nums: Vec<i32>) -> i32 {
+    if nums.len() == 0 {
+        return 0
+    }
     let mut count = 1;
 	let hs :HashSet<i32> = HashSet::from_iter(nums);
 	println!("{:?}", hs);
 	for i in &hs{
-		let pre = (i + 1).abs();
-		if hs.contains(&pre) {
-			count += 1;
-		}
-	}
+        if !hs.contains(&(i - 1)) {
+            let mut length = 1;
+            let mut pre = i + 1;
+            while hs.contains(&pre) {
+                length += 1;
+                pre += 1;
+            }
+        
+        count = count.max(length);
+        }
+    }
     count
     }
 }
-*/
